8fa46c961c49dd7e5e92b72c9d75719d12b850fe
Phyb1 <phibeon@yahoo.com>
2026-07-22 12:00:31 +0200

    static fix

--- shato/urls.py
+++ shato/urls.py
@@ -20,3 +20,10 @@ urlpatterns = [
 # Serve user-uploaded media (e.g. notice flyers) in development.
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+	
+from django.urls import re_path
+from django.views.static import serve
+
+urlpatterns += [
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+]
